ad54cc5252e3a68075fbe269739af7440167a212
Ramandeep singh <76144050+RaMandeep2002@users.noreply.github.com>
2024-01-08 18:38:57 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -459,8 +459,13 @@ const App = () => {
   const [provider, setProvider] = useState(null);
   const [contract, setContract] = useState(null);
   const [account, setAccount] = useState('');
-  const [nftDetails, setNftDetails] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [walletAddress, setWalletAddress] = useState('');
+  const [nftName, setnftName] = useState('');
+  const [nftDescription, setnftDescription] = useState('')
+  const [nftAmount, setnftAmmount] = useState('')
+  const [nftQuanitiy, setnftQuanitiy] = useState('')
+  const [nftBaseTokenURI, setNftBaseTokenUri] = useState('')
+  const [nftAmountProvider, setnftAmountProvider] = useState('')
 
   // const [contractData, setContractData] = useState('');
 
@@ -494,6 +499,61 @@ const App = () => {
     init();
   }, []);
 
+  useEffect(() => {
+    getCurrentWalletConnected();
+    addWalletListener();
+  }, [walletAddress]);
+
+  const connectWallet = async () => {
+    if (typeof window != 'undefined' && typeof window.ethereum != 'undefined') {
+      try {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        setWalletAddress(accounts[0]);
+        // console.log(accounts[0]);
+      } catch (error) {
+        console.error(error.message);
+      }
+    } else {
+      alert('Please Install Metamask!');
+    }
+  };
+
+  const getCurrentWalletConnected = async () => {
+    if (typeof window != 'undefined' && typeof window.ethereum != 'undefined') {
+      try {
+        const accounts = await window.ethereum.request({
+          method: 'eth_accounts',
+        });
+        if (accounts.length > 0) {
+          setWalletAddress(accounts[0]);
+          // console.log(accounts[0]);
+        } else {
+          console.log('Connect to MetaMask using the Connect button');
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log('Please install MetaMask');
+    }
+  };
+  const addWalletListener = async () => {
+    if (typeof window != 'undefined' && typeof window.ethereum != 'undefined') {
+      window.ethereum.on('accountsChanged', (accounts) => {
+        setWalletAddress(accounts[0]);
+        // console.log(accounts[0]);
+      });
+    } else {
+      /* MetaMask is not installed */
+      setWalletAddress('');
+      console.log('Please install MetaMask');
+    }
+  };
+
+
   // const fetchDataFromContract = async () => {
   //   try {
   //     // Call a read-only function on the smart contract
@@ -504,36 +564,36 @@ const App = () => {
   //   }
   // };
 
-  const fetchNftDetails = async () => {
-    try {
-      setLoading(true);
+  // const fetchNftDetails = async () => {
+  //   try {
+  //     setLoading(true);
 
-      // Call the nftDetails function on the smart contract
-      const totalMinted = await contract.nextTokenId();
-      const nftDetailsArray = [];
+  //     // Call the nftDetails function on the smart contract
+  //     const totalMinted = await contract.nextTokenId();
+  //     const nftDetailsArray = [];
 
-      for (let i = 0; i <= totalMinted; i++) {
-        const nftDetail = await contract.nftDetails(i);
-        nftDetailsArray.push(nftDetail);
-      }
+  //     for (let i = 0; i <= totalMinted; i++) {
+  //       const nftDetail = await contract.nftDetails(i);
+  //       nftDetailsArray.push(nftDetail);
+  //     }
 
-      setNftDetails(nftDetailsArray);
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching NFT details:', error.message);
-      setLoading(false);
-    }
-  };
+  //     setNftDetails(nftDetailsArray);
+  //     setLoading(false);
+  //   } catch (error) {
+  //     console.error('Error fetching NFT details:', error.message);
+  //     setLoading(false);
+  //   }
+  // };
   const createNFt = async () => {
     try {
       // Send a transaction to the smart contract
       const transaction = await contract.createNft(
-        'asdfasdf',
-        'asdfasdf',
-        10000000,
-        3,
-        'sdfasdf',
-        '0xb62962Bb2661BC5c5fFfa648A2C2B7A3e8CD4d4B'
+        nftName,
+        nftDescription,
+        nftAmount,
+        nftQuanitiy,
+        nftBaseTokenURI,
+        nftAmountProvider
       );
       await transaction.wait();
       console.log('Transaction successful!');
@@ -543,21 +603,137 @@ const App = () => {
   };
 
   return (
-    <div>
-      <p>Connected Account: {account}</p>
-      {/* <p>Contract Data: {contractData}</p> */}
-      {/* <button onClick={fetchDataFromContract}>Fetch Data</button> */}
+    <>
+      <div>
+        <div className="mt-4 flex justify-center items-center gap-3">
+          <p className="text-3xl  font-bold leading-normal tracking-normal ml-4 bg-gradient-to-r from-teal-400 to-yellow-200 bg-clip-text text-transparent">
+            ANRYTON Nft Contract Deploy
+          </p>
+          <button className="w-auto h-[25px] p-5  bg-gradient-to-r from-amber-600 to-pink-600 rounded-[30px] justify-center items-center gap-2 inline-flex"   onClick={connectWallet}>
+            <span className="text-white text-md font-medium leading-normal tracking-wide">
+            {walletAddress && walletAddress.length > 0
+                ? `${walletAddress}`
+                : '🦊   Connect Wallet'}
+            </span>
+          </button>
+        </div>
 
-      <button
-        type="button"
-        className="w-auto h-[25px] p-5 ml-4 bg-gradient-to-r from-amber-600 to-pink-600 rounded-[30px] justify-center items-center gap-2 inline-flex"
-        onClick={createNFt}
-      >
-        <span className="text-white text-md font-medium  tracking-wide">
-          CreateNft 🛠️
-        </span>
-      </button>
-    </div>
+        {/* <br /> */}
+        <div className="flex mr-5 justify-center">
+          <div className="ml-4"></div>
+
+          <div className="ml-4">
+            <form className="w-[30rem] mt-4 p-8 border border-gray-600 rounded-3xl">
+              <div className="mb-4">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium tracking-wide text-gray-600"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="mt-1 p-2 border rounded-md border-gray-600 w-full"
+                  placeholder="Enter the nft name"
+                  onChange={(e) => setnftName(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="description"
+                  className="block text-sm font-medium tracking-wide text-gray-600"
+                >
+                  Description
+                </label>
+                <textarea
+                  type="text"
+                  id="description"
+                  className="mt-1 p-2 border rounded-md border-gray-600 w-full"
+                  placeholder="Enter a decrption of NFT"
+                  onChange={(e) => setnftDescription(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="amount"
+                  className="block text-sm font-medium text-gray-600"
+                >
+                  Amount
+                </label>
+                <input
+                  type="number"
+                  id="amount"
+                  className="mt-1 p-2 border rounded-md border-gray-600 w-full"
+                  placeholder="Enter a Amount of NFT"
+                  onChange={(e) => setnftAmmount(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="quantity"
+                  className="block text-sm font-medium text-gray-600"
+                >
+                  Quantity
+                </label>
+                <input
+                  type="number"
+                  id="quantity"
+                  className="mt-1 p-2 border rounded-md border-gray-600 w-full"
+                  placeholder="Enter a Quantity"
+                  onChange={(e) => setnftQuanitiy(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="baseTokenURI"
+                  className="block text-sm font-medium text-gray-600"
+                >
+                  Base Token URI 🔗
+                </label>
+                <input
+                  type="text"
+                  id="baseTokenURI"
+                  className="mt-1 p-2 border rounded-md border-gray-600 w-full"
+                  placeholder="Enter a Base URI"
+                  onChange={(e) => setNftBaseTokenUri(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="amountProvider"
+                  className="block text-sm font-medium text-gray-600"
+                >
+                  Amount Provider
+                </label>
+                <input
+                  type="text"
+                  id="amountProvider"
+                  className="mt-1 p-2 border rounded-md border-gray-600 w-full"
+                  placeholder="Enter a address Amount Provider"
+                  onChange={(e) => setnftAmountProvider(e.target.value)}
+                />
+              </div>
+
+              <button
+                type="button"
+                className="w-auto h-[25px] p-5 ml-4 bg-gradient-to-r from-amber-600 to-pink-600 rounded-[30px] justify-center items-center gap-2 inline-flex"
+                onClick={createNFt}
+              >
+                <span className="text-white text-md font-medium  tracking-wide">
+                  CreateNft 🛠️
+                </span>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
